6c258a27479d78e003074509710f776d206e42ab
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-10-30 05:00:54 -0400

    commit tree 58ece6c13969d4e0d0e11bfac99bb3f871a71246

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,21 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+	handleClick = (event) => {
+		const coords = [event.clientX, event.clientY]
+		this.props.onReceiveCoordinates(coords)
+	}
+
+
+	render() {
+		return(
+			<button onClick={this.handleClick}>
+
+			</button>
+		)
+	}
+}
+
+export default CoordinatesButton
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,21 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+
+	handleClick = (event) => {
+		event.persist()
+		setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+		// console.log(this.props.onDelayedClick(clickEvent))
+	}
+
+	render() {
+		return(
+			<button onClick={this.handleClick}>
+
+			</button>
+		)
+	}
+}
+
+export default DelayedButton
